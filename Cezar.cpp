3bc3231bf411ac7f7653c1d57fbfac1dda86f799
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2021-11-18 21:55:16 +0300

    Add files via upload

--- Cezar.cpp
+++ Cezar.cpp
@@ -1,22 +1,24 @@
 #include "middle_str.h"
 
-string itc_Cezar(string str, int k)
+string itc_Cezar(string str, int s)
 {
     if(str.empty())
         return "";
     string re = "";
+    long long k = s;
     long long i = 0, len = itc_len(str);
     char charr = 0;
     //k = 1;
-    while (k > 27)
-        k = k - 27;
+    //while (k > 27)
+    //    k = k - 26;
+    k = k % 26;
     while (i < len)
     {
         if((str[i] >= 97 && str[i] <= 122) || (str[i] >= 65 && str[i] <= 90))
             charr = str[i] + k;
         else
             charr = str[i];
-        if(str[i] >= 97 && str[i] <= 122)
+        /*if(str[i] >= 97 && str[i] <= 122)
         {
             if(charr < 97)
             {
@@ -50,6 +52,7 @@ string itc_Cezar(string str, int k)
                 }
             }
         }
+        */
         re.push_back(charr);
         i++;
     }
