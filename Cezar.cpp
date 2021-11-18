84b0cb634de1632ea422939d3006f6833304bcfa
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2021-11-18 22:00:55 +0300

    Add files via upload

--- Cezar.cpp
+++ Cezar.cpp
@@ -9,9 +9,9 @@ string itc_Cezar(string str, int s)
     long long i = 0, len = itc_len(str);
     char charr = 0;
     //k = 1;
-    //while (k > 27)
-    //    k = k - 26;
-    k = k % 26;
+    while (k >= 26)
+        k = k - 26;
+    //k = k % 27;
     while (i < len)
     {
         if((str[i] >= 97 && str[i] <= 122) || (str[i] >= 65 && str[i] <= 90))
